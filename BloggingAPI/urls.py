def48225654c95b2e1a7618d072b80a07920e769
kchow3 <kchow3@ualberta.ca>
2016-02-23 22:17:14 -0700

    added url code for routing API in urls.py

--- BloggingAPI/urls.py
+++ BloggingAPI/urls.py
@@ -1,11 +1,36 @@
 from django.conf.urls import patterns, url, include
 from rest_framework import routers
+from rest_framework_nested import routers
 from django.contrib import admin
 from .views import *
 
 
-router = routers.SimpleRouter()
-router.register(r'author', AuthorViewSet)
+apiRouter = routers.DefaultRouter()
+
+# http://service/author (ONLY USED FOR TESTING)
+# http://service/author/{AUTHOR_ID}
+apiRouter.register(r'author', AuthorViewSet)
+
+# http://service/posts
+# http://service/posts/{POST_ID}
+# apiRouter.register(r'posts', PostsViewSet)
+
+# http://service/author/posts
+# apiRouter.register(r'author/posts', CurrentAuthorPostsViewSet)
+
+# http://service/author/{AUTHOR_ID}/posts
+# posts_router = routers.NestedSimpleRouter(apiRouter, r'author', lookup='author')
+# posts_router.register(r'posts', AuthorPostsViewSet)
+
+# http://service/posts/{post_id}/comments
+# comments_router = routers.NestedSimpleRouter(apiRouter, r'posts', lookup='posts')
+# comments_router.register(r'comments', PostCommentsViewSet)
+
+# http://service/friends/<authorid>
+# apiRouter.register(r'friends', FriendsViewSet)
+
+# http://service/friendrequest
+# apiRouter.register(r'friendrequest', FriendRequestViewSet)
 
 urlpatterns = [
     # Examples:
@@ -13,5 +38,7 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include(router.urls)),
+    url(r'^', include(apiRouter.urls)),
+    # url(r'^', include(posts_router.urls)),
+    # url(r'^', include(comments_router.urls)),
 ]
